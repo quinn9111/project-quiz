a14b9fb051a78551eb66f643ad5383e018f98529
graslowsnail <graslowsnail@gmail.,com>
2022-09-24 17:19:41 -0500

    got a random fact to print in the footer

--- assets/js/facts.js
+++ assets/js/facts.js
@@ -6,8 +6,25 @@ $.ajax({
     contentType: 'application/json',
     success: function (result) {
         console.log(result[0].fact);
+        var randomFact = result[0].fact;
+        displayFact(randomFact);
+
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
 });
+
+
+var displayFact = function (randomFact) {
+    var factLi = $("<li>")
+        .addClass("");
+    var factP = $("<p>")
+        .addClass("m-1")
+        .text(randomFact);
+    factLi.append(factP);
+
+    $("#randomFact").append(factLi);
+
+
+};
